0b6a8753af4b44c0f1f3530e2b3aa92979e31e0a
Gizem <gizem.ay1996@gmail.com>
2021-12-02 23:57:07 +0300

    email validations

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
-const anchors = ['#home-section-link', '#about-section-link' , '#contact-link'];
+const anchors = ['#home-section-link', '#about-section-link', '#contact-link'];
+const formValues = ['name', 'subject', 'email', 'message'];
+$("#contactForm").submit(function (e) {
+     e.preventDefault();
+});
 
+
+// Navigation in page
 function getSection(e) {
-   console.log(e)
      setTimeout(() => {
           const hash = window.location.hash;
           const formattedHash = `${hash}-link`;
@@ -17,20 +22,72 @@ function getSection(e) {
 }
 
 
+// Email Sending Func
 const inputs = document.querySelector('form');
-function setEmail(){
+function setEmail() {
+     document.getElementById('submit').style.display = 'none'
+     document.getElementById('submitting').style.opacity = 1;
+
+
      var templateParams = {
           from_name: inputs.elements['name'].value,
-          to_name: 'Eda Balcı',
           subject_info: inputs.elements['subject'].value,
           email: inputs.elements['email'].value,
           message: inputs.elements['message'].value
-      };
+     };
 
-      emailjs.send('gmail', 'template_iz81l8p', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+     const formErrors = isFormValid(templateParams);
+
+     if (formErrors.length) {
+          formErrors.forEach(errorKey => {
+               document.getElementById(errorKey).style.display = 'block'
+          });
+          document.getElementById('submit').style.display = 'block'
+          document.getElementById('submitting').style.opacity = 0;
+     } else {
+          emailjs.send('gmail', 'template_iz81l8p', templateParams)
+               .then(function (response) {
+                    console.log('SUCCESS!', response.status, response.text);
+               }, function (error) {
+                    console.log('FAILED...', error);
+               });
+          setTimeout(() => {
+               formValues.forEach(valueKey => {
+                    document.getElementById(valueKey).value = ' '
+               });
+               $('.toast').toast('show');
+               document.getElementById('submit').style.display = 'block'
+               document.getElementById('submitting').style.opacity = 0;
+          }, 1000);
+
+     }
+
 }
+
+// Form Controls
+function isFormValid(templateParams) {
+     let errors = []
+     if (validateEmail(templateParams.email) === null) {
+          errors.push('inv-mail')
+     }
+     if (!templateParams.from_name.length) {
+          errors.push('inv-name')
+
+     }
+     if (!templateParams.message.length) {
+          errors.push('inv-message')
+     }
+     return errors;
+}
+
+
+const validateEmail = (email) => {
+     return String(email)
+          .toLowerCase()
+          .match(
+               /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+};
+
+
+
